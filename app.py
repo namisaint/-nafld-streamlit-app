0b66f681b07e10c30f1a0a01acc1eb376f4b4ba2
namisaint <u2830447@uel.ac.uk>
2025-08-11 07:31:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 st.set_page_config(page_title="NAFLD Hello", page_icon="👋")
 st.title("👋 Hello from Streamlit")
-st.write("If you can read this, the build finished. Next we’ll add the model.")
+st.write("If you can read this, the build finished. We'll add the model next.")
